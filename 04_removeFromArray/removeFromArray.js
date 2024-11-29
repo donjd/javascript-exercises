b3f940a4c4ba2e62489f5f0a07be82b642c0d5c8
donjd <99505859+donjd@users.noreply.github.com>
2024-11-29 10:24:32 -0700

    Try fixing removeFromArray, did not work

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...itemsToRemove) {
+  const filteredArray = array.filter((element) => element == itemsToRemove);
+  return filteredArray;
 };
+
+// const removeFromArray = function (array, ...itemsToRemove) {
+//   for (i = 0; i < itemsToRemove.length - 1; i++) {
+//     const index = array.indexOf(itemsToRemove);
+//     array.splice(index, 1);
+//   }
+//   return array;
+// };
+
+// const removeFromArray = function (array, itemsToRemove) {
+//   const index = array.indexOf(itemsToRemove);
+//   array.splice(index, 1);
+//   return array;
+// };
 
 // Do not edit below this line
 module.exports = removeFromArray;
