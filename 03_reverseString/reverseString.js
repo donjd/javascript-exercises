170afb44b6b8c3e85e54fb88bfdc5e7696414673
donjd <99505859+donjd@users.noreply.github.com>
2024-11-28 22:56:15 -0700

    Write reverseString function and pass tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (input) {
+  chars = input.split("");
+  reversedString = "";
+  for (let i = chars.length - 1; i >= 0; i--) {
+    reversedString += chars[i];
+  }
+  return reversedString;
 };
 
 // Do not edit below this line
