d4672da8b36f11a7a65eb0fae384d4d3db573aff
donjd <99505859+donjd@users.noreply.github.com>
2024-11-29 20:23:59 -0700

    Function sums numbers, not sum all in range. Need fixing.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function (...numbers) {
+  const sum = numbers.reduce((previous, next) => previous + next);
+  console.log(sum);
 };
 
+sumAll(1, 4);
+
 // Do not edit below this line
-module.exports = sumAll;
+// module.exports = sumAll;
