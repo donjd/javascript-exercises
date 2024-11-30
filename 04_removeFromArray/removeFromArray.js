1f9045542eb0bdb9c46e4fdc0cb47972f5463c26
donjd <99505859+donjd@users.noreply.github.com>
2024-11-29 20:16:40 -0700

    Use filter and includes to removeFromArray and pass test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,21 +1,9 @@
 const removeFromArray = function (array, ...itemsToRemove) {
-  const filteredArray = array.filter((element) => element == itemsToRemove);
+  const filteredArray = array.filter(
+    (element) => !itemsToRemove.includes(element)
+  );
   return filteredArray;
 };
-
-// const removeFromArray = function (array, ...itemsToRemove) {
-//   for (i = 0; i < itemsToRemove.length - 1; i++) {
-//     const index = array.indexOf(itemsToRemove);
-//     array.splice(index, 1);
-//   }
-//   return array;
-// };
-
-// const removeFromArray = function (array, itemsToRemove) {
-//   const index = array.indexOf(itemsToRemove);
-//   array.splice(index, 1);
-//   return array;
-// };
 
 // Do not edit below this line
 module.exports = removeFromArray;
