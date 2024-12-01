625dc15c3672d8082cba870b3a4edb40394e6826
donjd <99505859+donjd@users.noreply.github.com>
2024-12-01 14:42:53 -0700

    Passess all tests except multiple words
    For some reason, even though a comma is not in
    the acceptableChars array, commas are slipping
    through the filter and causing bugs.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,56 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const ALPHABET = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  const DIGITS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const acceptableChars = ALPHABET.join(DIGITS);
+  let chars = string
+    .toLowerCase()
+    .split("")
+    .filter((element) => acceptableChars.includes(element))
+    .reduce((prev, curr) => prev + curr);
+  let reversedChars = string
+    .toLowerCase()
+    .split("")
+    .filter((element) => acceptableChars.includes(element))
+    .reverse()
+    .reduce((prev, curr) => prev + curr);
 
+  console.log(chars);
+  console.log(reversedChars);
+  if (chars == reversedChars) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+console.log(palindromes("a car, a man, a maraca"));
