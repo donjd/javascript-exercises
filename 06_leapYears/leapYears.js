6cf1b6c1878d030c25998551a602e1b9cf970a80
donjd <99505859+donjd@users.noreply.github.com>
2024-11-30 20:47:18 -0700

    Solve leapYears function with switch statements

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,17 +1,22 @@
 const leapYears = function (year) {
-  if (year % 4 == 0) {
-    if (year % 100 == 0) {
-      if (year % 400 == 0) {
-        return true;
-      } else {
-        return false;
-      }
-    } else {
+  switch (true) {
+    case year % 400 == 0:
       return true;
-    }
-  } else {
-    return false;
+      break;
+    case year % 4 == 0:
+      switch (true) {
+        case year % 100 == 0:
+          return false;
+          break;
+        default:
+          return true;
+          break;
+      }
+    default:
+      return false;
+      break;
   }
 };
+
 // Do not edit below this line
 module.exports = leapYears;
