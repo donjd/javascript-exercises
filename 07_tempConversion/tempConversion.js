13c8ca1210812f17e23a7fe162684e51416ee76c
donjd <99505859+donjd@users.noreply.github.com>
2024-11-30 21:28:31 -0700

    Solve tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempInF) {
+  let celcius = ((tempInF - 32) * 5) / 9;
+  return Number(celcius.toFixed(1));
+  // console.log(celcius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempInC) {
+  let fahrenheit = tempInC * (9 / 5) + 32;
+  return Number(fahrenheit.toFixed(1));
+  // console.log(fahrenheit.toFixed(1));
 };
+
+convertToCelsius(0);
+convertToFahrenheit(0);
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
