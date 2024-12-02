277f92e08b1985cbdc876c790f2b3b90188e1090
donjd <99505859+donjd@users.noreply.github.com>
2024-12-02 16:51:58 -0700

    Solve fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (index) {
+  let sum = 0;
+  let sequence = [0, 1];
+  if (index < 0) {
+    return "OOPS";
+  } else {
+    for (let i = 1; i <= index; i++) {
+      let last = sequence.at(-1);
+      let secondLast = sequence.at(-2);
+      sum = last + secondLast;
+      sequence.push(sum);
+    }
+  }
+  return sequence[Number(index)];
 };
 
 // Do not edit below this line
