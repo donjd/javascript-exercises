548d8eb23c20a4cf920ea70dc8eb30710b06f2b1
donjd <99505859+donjd@users.noreply.github.com>
2024-12-02 16:59:11 -0700

    Solve getTheTitles function with array.map

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (array) {
+  const bookTitles = array.map((element) => element.title);
+  return bookTitles;
 };
 
 // Do not edit below this line
