094c123a22e862b9f98369c52351161ff0b38587
donjd <99505859+donjd@users.noreply.github.com>
2024-12-03 22:27:17 -0700

    Solve findTheOldest function
    This one was really difficult for me. Here are
    some main takeaways I got from this exercise:
    * array.map creates a new array with some
    modifications from a previously existing array.
    * The ternary operator comes in handy with the
    array.reduce method. Very nice way to find max
    number without using Math.max()
    * I need to learn more about Date function and
    getTime, getFullYear, etc.
    * the spread operator can be used to copy over
    all existing properties in a map method. Then add
    a comma and add a new property

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,62 +1,24 @@
-const people = [
-  {
-    name: "Carly",
-    yearOfBirth: 1942,
-    yearOfDeath: 1970,
-  },
-  {
-    name: "Ray",
-    yearOfBirth: 1962,
-    yearOfDeath: 2011,
-  },
-  {
-    name: "Jane",
-    yearOfBirth: 1912,
-    yearOfDeath: 1941,
-  },
-];
-
-const peopleWithAge = people.map((person) => ({
-  ...person,
-  age: getAge(person),
-}));
-
-function getAge(person) {
-  const today = new Date().getTime();
-  const birthYear = new Date(person.yearOfBirth).getTime();
-  const deathYear = new Date(person.yearOfDeath).getTime();
-  if (deathYear === undefined) {
-    return today - birthYear;
-  } else {
-    return deathYear - birthYear;
+const findTheOldest = function (people) {
+  function getAge(person) {
+    const currentYear = new Date().getFullYear();
+    if (person.yearOfDeath === undefined) {
+      return currentYear - person.yearOfBirth;
+    } else {
+      return person.yearOfDeath - person.yearOfBirth;
+    }
   }
-}
 
-console.log(peopleWithAge[2].age);
+  const peopleWithAge = people.map((person) => ({
+    ...person,
+    age: getAge(person),
+  }));
 
-// const findTheOldest = function (people) {
-//   const getAge = function (person) {
-//     let today = new Date().getTime();
-//     const birthYear = new Date(person.yearOfBirth).getTime();
-//     const deathYear = new Date(person.yearOfDeath).getTime();
-//     if (deathYear === undefined) {
-//       return today - birthYear;
-//     } else {
-//       return deathYear - birthYear;
-//     }
-//   };
+  const oldestPerson = peopleWithAge.reduce((prev, curr) => {
+    return prev.age > curr.age ? prev : curr;
+  });
 
-//   const peopleWithAge = people.map((person) => {
-//     name: person.name;
-//     yearOfBirth: person.yearOfBirth;
-//     yearOfDeath: person.yearOfDeath;
-//     age: getAge(person);
-//   });
-
-//   console.log(peopleWithAge);
-// };
-
-// findTheOldest(people);
+  return oldestPerson;
+};
 
 // Do not edit below this line
-// module.exports = findTheOldest;
+module.exports = findTheOldest;
